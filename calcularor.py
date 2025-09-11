bef60744a209537996a4e8e4b0892400260d1ee1
mary1924 <masha.marunchak@gmail.com>
2025-09-11 09:33:48 +0300

    Add files via upload

---
+++ calcularor.py
@@ -0,0 +1,18 @@
+
+
+num1 = int(input('first number:'))
+num2 = int(input('second number:'))
+
+print("action:+, -, /, *")
+
+
+a= input("chose action: ")
+
+if a == "+":
+    print("Your result:", num1+num2)
+if a =="-":
+    print("Your result:", num1-num2)
+elif a =="/":
+    print("Your result:", num1/num2)
+elif a=="*":
+    print("Your result:", num1*num2)
